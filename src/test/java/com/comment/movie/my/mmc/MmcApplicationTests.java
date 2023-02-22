26dd4aca3585278b7a34e323bdde02ad285b6cd6
sunnnkim <ksunju0610@naver.com>
2023-02-22 22:18:25 +0900

    init

---
+++ src/test/java/com/comment/movie/my/mmc/MmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comment.movie.my.mmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
